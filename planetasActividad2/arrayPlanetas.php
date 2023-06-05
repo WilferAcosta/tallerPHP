5792ccb4d6e1e86f59d3d1f9f05342f468bfec39
WilferAcosta <w.o.a.r@hotmail.es>
2023-06-05 16:01:24 -0500

    feat: :construction: actividad#2
    agrega la solucion de la actividad 2

--- planetasActividad2/arrayPlanetas.php
+++ planetasActividad2/arrayPlanetas.php
@@ -1,23 +1,16 @@
 <?php 
 echo <<<HTML
-el planeta buscado fue :<br> <a href="indexPlaneta.html">Volver</a>
+Los planetas creados fueron :<br> <a href="indexPlaneta.html">Volver</a>
 HTML;
-$sistemaSolar = array(
-    "sol" => 1,
-    "mercurio" => 2,
-    "venus" => 3,
-    "tierra" => 4,
-    "marte" => 5,
-    "jupiter" => 6,
-    "saturno" => 7,
-    "urano" => 8,
-    "neptuno" => 9,
-    "pluton" => 10
-);
+$numeroPlaneta='deshabilitado';
 if (isset($_POST)) {
-$buscar = $_POST['numero'];
-$ordenados=array_flip($sistemaSolar);
-$resultado = $ordenados[$buscar];
-echo "<h1>$resultado</h1>";
+    $numeroPlaneta=$_POST["cantidad"];
+    $sistema = array_fill(0, $numeroPlaneta, 'planeta');
+    //var_dump($sistema);
+    function imprimir($planeta){
+    echo "$planeta...\n";
+    };
+    echo"<br>";
+    array_walk($sistema,'imprimir');
 }
 ?>
