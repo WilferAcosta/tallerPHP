cc2895b6fc856d7d2aa9bae89278ce09615be996
WilferAcosta <w.o.a.r@hotmail.es>
2023-06-06 20:19:49 -0500

    feat: :construction: actividad#9
    Eliminar elementos duplicados de una lista de especies alienígenas

---
+++ planetasActividad9/arrayPlanetas.php
@@ -0,0 +1,9 @@
+<?php 
+echo <<<HTML
+Los planetas creados fueron :<br> <a href="indexPlaneta.html">Volver</a><br>
+HTML;
+$alienigenas = ["Zorg", "Xenon", "Zorg", "Gloob", "Zog", "Xenon", "Gloob", "Zog", "Ypsilon", "Xenon"];
+
+        
+        var_dump(array_unique($alienigenas));
+?>
